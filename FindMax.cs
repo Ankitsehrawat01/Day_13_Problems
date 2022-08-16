b36b42c9a614e9b053d5b1d4a95ba052d47d7eb7
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-16 20:34:29 +0530

    max integer program

---
+++ FindMax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Practice_Problem
+{
+    public class FindMax
+    {
+        public int FindMxInteger(int First, int Second, int Third)
+        {
+            if (First.CompareTo(Second) > 0 && First.CompareTo(Third) > 0 )
+            {
+                return First;
+            }
+            if (Second.CompareTo(Third) > 0 &&  Second.CompareTo(First) > 0 )
+            {
+                return Second;
+            }
+            if (Third.CompareTo(First) > 0 && Third.CompareTo(Second) > 0 )
+            {
+                return Third;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Day_13_Practice_Problem;
+namespace Day_13_Practice_Problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool check = true;
+            FindMax obj1 = new FindMax();
+            while (check)
+            {
+                Console.Write("Select Option:\n" +
+                    "1)Find Integer Maximum Number\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        int result = obj1.FindMxInteger(4, 3, 2);
+                        Console.WriteLine(result);
+                        Console.WriteLine("-----------------------------------");
+                        Console.WriteLine("Maximum Number is:" + result);// To display Maximum Number
+                        Console.WriteLine("-----------------------------------");
+                        break;
+                    default:
+                        check = !check;
+                        break;
+                }
+            
+            }
+        }
+    }
+}
