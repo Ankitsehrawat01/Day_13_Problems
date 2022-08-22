f92b45967c1ec4a0c2c685e457151c3183d6c396
Ankitsehrawat01 <sehrawata328@gmail.com>
2022-08-22 16:16:42 +0530

    maximum using generic class and sort method

--- Maximum.cs
+++ Maximum.cs
@@ -6,25 +6,24 @@ using System.Threading.Tasks;
 
 namespace Day_13_Practice_Problem
 {
-    public class Maximum
+    public class Maximum<T>
     {
-        //for integer
-        public int[] value;
-        public Maximum(int[] value)
+        public T[] value;
+        public Maximum(T[] value)
         {
             this.value = value;
         }
-        public int[] Sort(int[] value)
+        public T[] Sort(T[] value)
         {
             Array.Sort(value);
             return value;
         }
-        public int MaxValues(params int[] values)
+        public T MaxValues(params T[] values)
         {
             var sorted_values = Sort(values);
             return sorted_values[^1];
         }
-        public int MaxMethod()
+        public T MaxMethod()
         {
             var max = MaxValues(this.value);
             return max;
@@ -32,59 +31,9 @@ namespace Day_13_Practice_Problem
         public void PrintMaxValue()
         {
             var max = MaxValues(this.value);
-            Console.WriteLine("Maximum integer value is " + max);
-        }
-        //for float
-        public float[] value1;
-        public Maximum(float[] value1)
-        {
-            this.value1 = value1;
-        }
-        public float[] Sort(float[] value1)
-        {
-            Array.Sort(value1);
-            return value1;
-        }
-        public float MaxValues(params float[] values)
-        {
-            var sorted_values = Sort(values);
-            return sorted_values[^1];
-        }
-        public float MaxMethod1()
-        {
-            var max1 = MaxValues(this.value1);
-            return max1;
-        }
-        public void PrintMaxValue1()
-        {
-            var max1 = MaxValues(this.value1);
-            Console.WriteLine("Maximum float value is " + max1);
-        }
-        //for string
-        public string[] value2;
-        public Maximum(string[] value2)
-        {
-            this.value2 = value2;
-        }
-        public string[] Sort(string[] value2)
-        {
-            Array.Sort(value2);
-            return value2;
-        }
-        public string MaxValues(params string[] values)
-        {
-            var sorted_values = Sort(values);
-            return sorted_values[^1];
-        }
-        public string MaxMethod2()
-        {
-            var max2 = MaxValues(this.value2);
-            return max2;
-        }
-        public void PrintMaxValue2()
-        {
-            var max2 = MaxValues(this.value2);
-            Console.WriteLine("Maximum string value is " + max2);
+            Console.WriteLine("---------");
+            Console.WriteLine("Maximum value is " + max);
+            Console.WriteLine("---------");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace Day_13_Practice_Problem
             while (check)
             {
                 Console.Write("Select Option:\n" +
-                    "1)Find Integer Maximum Number\n" + 
-                    "2)Find float Maximum Number\n" + 
-                    "3)Find Maximum String\n" + 
-                    "4)Find Maximum using sort Method\n");
+                    "1) Find Integer Maximum Number\n" + 
+                    "2) Find float Maximum Number\n" + 
+                    "3) Find Maximum String\n" + 
+                    "4) Find Maximum integer using sort Method\n" +
+                    "5) Find Maximum float using sort Method\n" +
+                    "6) Find Maximum string using sort Method\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -43,16 +45,20 @@ namespace Day_13_Practice_Problem
                     case 4:
                         //for integer
                         int[] intArray = { 12, 15, 17, 13, 14, 19, 222 };
-                        Maximum obj = new Maximum(intArray);
+                        Maximum<int> obj = new Maximum<int>(intArray);
                         obj.PrintMaxValue();
+                        break;
+                    case 5:
                         //for float
                         float[] floatArray = { 12.2f, 15.4f, 17.1f, 13.9f };
-                        Maximum obj4 = new Maximum(floatArray);
-                        obj4.PrintMaxValue1();
+                        Maximum<float> obj4 = new Maximum<float>(floatArray);
+                        obj4.PrintMaxValue();
+                        break;
+                    case 6:
                         //for string
                         string[] stringArray = { "a", "b", "c", "d", "e" };
-                        Maximum obj5 = new Maximum(stringArray);
-                        obj5.PrintMaxValue2();
+                        Maximum<string> obj5 = new Maximum<string>(stringArray);
+                        obj5.PrintMaxValue();
                         break;
                 }
             }
